b7a7be57568a0f5b32c54e0b4f7a83934ea73f49
pixelshade <peter.sulik@cellense.com>
2018-04-17 21:16:11 +0200

    fix line shifts, add g to worksheet names

--- main.py
+++ main.py
@@ -34,7 +34,14 @@ def loadFiles():
       if(len(line) == 0 or len(line[0]) == ''):
         continue
       line = line[:2] + line[start:end]
-      line = [float(x) if(x != '()' and ':' not in x) else None for x in line]
+
+      for i,x in enumerate(line):
+        if(x == '()'):
+          line[i] = None
+        elif ':' in x:
+          line[i] = x
+        else:
+          line[i] = float(x)
       res.setdefault((prop, mms), []).append(line)
 
   return res
@@ -42,10 +49,12 @@ def loadFiles():
 
 def addAvgs(dataSht):
   lenline = 0
+  maxlenline = 0
   startLine = 4
   for i, line in enumerate(dataSht):
     line.insert(0,None)
     lenline = len(line)
+    maxlenline = max(maxlenline, lenline)
     endAlph = xlsxwriter.utility.xl_col_to_name(lenline-2)
     rowi = startLine + i
     stats = [
@@ -58,26 +67,26 @@ def addAvgs(dataSht):
     # print(len(line))
     dataSht[i] = stats + line
     # print(len(line))
-  lastLineI = len(dataSht)+2
+  firstLineI = len(dataSht)+2
 
-  padding = [None] * 8
-  lastLineAvgs = list(padding) + ['AVERAGE']
-  for i in range(len(lastLineAvgs), len(lastLineAvgs)+lenline):
+  padding = [None] * 7
+  firstLineAvgs = ['AVERAGE', 'MIN', 'MAX', None, None, None, None, 'AVERAGE']
+  for i in range(len(firstLineAvgs), len(firstLineAvgs) + maxlenline - 3):
     endAlph = xlsxwriter.utility.xl_col_to_name(i)
-    lastLineAvgs.append('=AVERAGE({}{}:{}{})'.format(endAlph, startLine, endAlph, lastLineI))
-  dataSht.insert(0, lastLineAvgs)
+    firstLineAvgs.append('=AVERAGE({}{}:{}{})'.format(endAlph, startLine, endAlph, firstLineI))
+  dataSht.insert(0, firstLineAvgs)
 
-  lastLineAvgs = list(padding) + ['MAX']
-  for i in range(len(lastLineAvgs), lenline):
+  firstLineAvgs = list(padding) + ['MAX']
+  for i in range(len(firstLineAvgs), len(firstLineAvgs) + maxlenline -3):
     endAlph = xlsxwriter.utility.xl_col_to_name(i)
-    lastLineAvgs.append('=MAX({}{}:{}{})'.format(endAlph, startLine, endAlph, lastLineI))
-  dataSht.insert(0, lastLineAvgs)
+    firstLineAvgs.append('=MAX({}{}:{}{})'.format(endAlph, startLine, endAlph, firstLineI))
+  dataSht.insert(0, firstLineAvgs)
 
-  lastLineAvgs = list(padding) + ['MIN']
-  for i in range(len(lastLineAvgs), lenline):
+  firstLineAvgs = list(padding) + ['MIN']
+  for i in range(len(firstLineAvgs), len(firstLineAvgs) + maxlenline - 3):
     endAlph = xlsxwriter.utility.xl_col_to_name(i)
-    lastLineAvgs.append('=MIN({}{}:{}{})'.format(endAlph, startLine, endAlph, lastLineI))
-  dataSht.insert(0, lastLineAvgs)
+    firstLineAvgs.append('=MIN({}{}:{}{})'.format(endAlph, startLine, endAlph, firstLineI))
+  dataSht.insert(0, firstLineAvgs)
 
   return dataSht
 
@@ -87,12 +96,15 @@ def addConditionalFormatting(worksheet, dataSht):
   colName = xlsxwriter.utility.xl_col_to_name(cols)
   end = '{}{}'.format(colName, rows)
   # main raw data
-  worksheet.conditional_format('I5:{}'.format(end), {'type': '3_color_scale'})
+  worksheet.conditional_format('I4:{}'.format(end), {'type': '3_color_scale'})
   # top stats
-  worksheet.conditional_format('I1:{}3'.format(colName), {'type': '3_color_scale'})
+  worksheet.conditional_format('I1:{}1'.format(colName), {'type': '3_color_scale'})
+  worksheet.conditional_format('I2:{}2'.format(colName), {'type': '3_color_scale'})
+  worksheet.conditional_format('I3:{}3'.format(colName), {'type': '3_color_scale'})
   # left stats
-  worksheet.conditional_format('A4:C{}'.format(rows), {
-                               'type': '3_color_scale'})
+  worksheet.conditional_format('A4:A{}'.format(rows), {'type': '3_color_scale'})
+  worksheet.conditional_format('B4:B{}'.format(rows), {'type': '3_color_scale'})
+  worksheet.conditional_format('C4:C{}'.format(rows), {'type': '3_color_scale'})
 
 def findBorders(lines):
   half = len(lines) / 2
@@ -114,13 +126,13 @@ def findBorders(lines):
 
 def createExcel(data):
   print('creating excel')
-  workbook = xlsxwriter.Workbook('hello.xlsx')
+  workbook = xlsxwriter.Workbook('data_eval.xlsx')
   for key in data:
     bar = progressbar.ProgressBar()
 
     row = 0
     col = 0
-    worksheet = workbook.add_worksheet(key[0])
+    worksheet = workbook.add_worksheet('{}_{}'.format(key[0], key[1]))
     data[key] = addAvgs(data[key])
     for line in bar(data[key]):
       worksheet.write_row(row, 0, line)
